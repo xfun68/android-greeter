8b5e398941db02b916cb8edf21c1c6379fa44536
Xiaofeng Wang <xfun68@gmail.com>
2013-06-16 22:44:05 +0300

    Greeting from the Server.

--- Greeter/build/source/r/debug/com/panda/greeter/R.java
+++ Greeter/build/source/r/debug/com/panda/greeter/R.java
@@ -18,11 +18,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f050004;
+        public static final int action_settings=0x7f050005;
         public static final int greetToPerson=0x7f050000;
         public static final int greetToWorld=0x7f050001;
         public static final int greeting=0x7f050002;
         public static final int personName=0x7f050003;
+        public static final int serverGreet=0x7f050004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,6 +39,7 @@ public final class R {
         public static final int greeting=0x7f060004;
         public static final int hello_world=0x7f060005;
         public static final int personName=0x7f060006;
+        public static final int serverGreets=0x7f060007;
     }
     public static final class style {
         /** 
--- Greeter/src/main/java/com/panda/greeter/MainActivity.java
+++ Greeter/src/main/java/com/panda/greeter/MainActivity.java
@@ -1,11 +1,23 @@
 package com.panda.greeter;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
     private TextView greetingView;
@@ -38,5 +50,76 @@ public class MainActivity extends Activity {
         Person person = new Person(name);
 
         greetingView.setText(greeter.greetToPerson(person));
+    }
+
+    public void serverGreet(View view) throws IOException {
+        String result = "Not connected.";
+        String urlString = "http://example.iana.org/";
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebPageTask().execute(urlString);
+        } else {
+            greetingView.setText("No network connection available.");
+        }
+    }
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            greetingView.setText(result);
+        }
+
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+
+        public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
     }
 }
